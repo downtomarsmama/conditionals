53aa0afd3c093f5c1e88425bbaee2870bd7bb8c6
downtomarsmama <54377186+downtomarsmama@users.noreply.github.com>
2020-03-02 10:02:52 -0500

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,54 @@
+//conditionals
+
+if(2+2 === 6){
+    console.log("Math works!");
+  } else {
+    console.log("Math is broken.")
+  }
+  
+  // Expected output: "Math works!"
+
+
+  myName = "Kayla";
+
+  if(myName === "Jacob"){
+console.log("Hi Two Stackzzz");
+  } else if (myName === "Walker"){
+      console.log("Hi Walker the Great!!")
+  } else if (myName === "Kayla"){
+      console.log("Gimme Coffee");
+  }
+   else {
+      console.log("Hi person with no face");
+  }
+
+var likestoTravel = true;
+if(likestoTravel === true){
+    console.log("Bon Voyage!");}
+else{
+        console.log("Enjoy your couch!");
+    }
+
+
+var favoriteFood = "tacos";
+if(favoriteFood === "fried pickels" || favoriteFood ==="Pepperoni rolls"){
+    console.log("congradulations! You have exellent taste!");}
+    else{
+        console.log("Clearly you have not tried fried pickles or pepperoni rolls.");
+    }
+
+if(1+1 >5)
+{console.log("true facts!");}
+else {console.log("LIES!");}
+if(4+5 < 20)
+{console.log("true facts!");}
+else{console.log("LIES!")}
+if(6+2>=0)
+{console.log("true fatcs!");}
+else {console.log("lies");} 
+
+var likesDogs = "true";
+if(likesDogs ==="true")
+{console.log("You're a dog person!");}
+else(console.log("Your're a cat person!"));
+
